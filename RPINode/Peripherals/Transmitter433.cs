726f5b687382b99424218605731a392d3729928b
jqt3of5 <jqt3of5@gmail.com>
2021-06-06 10:08:45 -0600

    Trying to make the transmitter accurate enough

--- RPINode/Peripherals/Transmitter433.cs
+++ RPINode/Peripherals/Transmitter433.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Node.Abstractions;
 using Unosquare.PiGpio.ManagedModel;
@@ -7,7 +8,7 @@ using Unosquare.RaspberryIO.Abstractions;
 
 namespace RPINode
 {
-    public record RadioSymbol(TimeSpan Duration, bool Value, int? Samples = null);
+    public record RadioSymbol(int DurationUS, bool Value, int? Samples = null);
     
     public class Transmitter433
     {
@@ -20,13 +21,20 @@ namespace RPINode
             pin.PinMode = GpioPinDriveMode.Output;
         }
 
-        public async Task Transmit(RadioSymbol[] symbols)
+        public Task Transmit(RadioSymbol[] symbols)
         {
-            foreach (var symbol in symbols)
+            return Task.Run(() =>
             {
-                _pin.Write(symbol.Value);
-                await Task.Delay(symbol.Duration);
-            }  
+                var sw = new Stopwatch();
+                sw.Start();
+                foreach (var symbol in symbols)
+                {
+                    _pin.Write(symbol.Value);
+                    var ticks = Stopwatch.Frequency / 100000 * symbol.DurationUS;
+                    while (sw.ElapsedTicks < ticks);
+                }  
+                _pin.Write(false);
+            });
         }
     }
 }
