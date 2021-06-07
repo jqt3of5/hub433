636d0f25e9960c19a11e51a6f204216b5e390989
jqtodd@xactware.com <jqtodd@xactware.com>
2021-06-06 21:22:22 -0600

    added bit strings

--- Node.Hardware/Peripherals/BlindsDevice.cs
+++ Node.Hardware/Peripherals/BlindsDevice.cs
@@ -21,7 +21,20 @@ namespace RPINode.Peripherals
 
         public async Task OpenOneStep()
         {
-            var bits = "110111010011011010100001110001000110110001";
+
+            //                 something    channel            ch crc  co crc 
+            //                     |           |      command      |   |
+            var upCh1   = "11011101001101101 1000 0011 1000 1000 0011 1000 1";
+            var stopCh1 = "11011101001101101 1000 1010 1000 0000 0011 1001 1";
+            var downCh1 = "11011101001101101 1000 1000 1000 1000 0011 0011 1";
+            var upCh2   = "11011101001101101 0100 0011 1000 1000 1101 1000 1";
+            var stopCh2 = "11011101001101101 0100 1010 1000 0000 1101 1001 1";
+            var downCh2 = "11011101001101101 0100 1000 1000 1000 1101 0011 1";
+
+            var fullUp  = "11011101001101101 0100 0011 1000 0000 1101 0100 1";
+            var fullDon = "11011101001101101 0100 1000 1000 0000 1101 1011 1";
+            
+            
             var data = bits.SelectMany(bit => 
                 bit == '1' ? 
                     new [] {new RadioSymbol(725, true), new RadioSymbol(350, false)} : 
@@ -29,9 +42,14 @@ namespace RPINode.Peripherals
             ).ToList();
 
             var buffer = _startPattern.Concat(data);
-            var buffer2 = _repeatPattern.Concat(data);
-            
-            await _transmitter433.Transmit(buffer.Concat(buffer2).Concat(buffer2).Concat(buffer2).Concat(buffer2).Concat(buffer2).Concat(buffer2).ToArray()); 
+            var buffer2 = _repeatPattern.Concat(data).ToArray();
+
+            for (int i = 0; i < 10; ++i)
+            {
+                buffer = buffer.Concat(buffer2);
+            }
+
+            await _transmitter433.Transmit(buffer.ToArray());
         }
 
         public async Task OpenFull()
