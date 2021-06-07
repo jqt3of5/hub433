7b227158fa707637f9a844ddcf6838f4c8c873e2
jqtodd@xactware.com <jqtodd@xactware.com>
2021-06-06 21:23:44 -0600

    get the csprojects

---
+++ Node.Tests.Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using RPINode;
+using RPINode.Peripherals;
+using Unosquare.PiGpio;
+using Unosquare.PiGpio.NativeEnums;
+using Unosquare.PiGpio.NativeMethods;
+
+namespace Node.Tests.Console
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (Setup.GpioInitialise() < 0)
+            {
+                System.Console.WriteLine("Failed to initialize. was your libpigpio.so built from a raspberrypi 4?");
+                Setup.GpioTerminate();
+                return;
+            }
+
+            var pin = Board.Pins[UserGpio.Bcm17];
+            var transmitter = new Transmitter433(pin);
+            var blinds = new BlindsDevice(transmitter);
+
+            await blinds.OpenOneStep();
+           
+            
+            Setup.GpioTerminate();
+        }
+    }
+}
---
+++ Node.Tests/TestTransmitter.cs
@@ -0,0 +1,36 @@
+using System.Linq;
+using System.Threading.Tasks;
+using HardwareAbstractionServiceRPI.Peripherals;
+using NUnit.Framework;
+using RPINode;
+using Unosquare.PiGpio;
+using Unosquare.PiGpio.NativeEnums;
+using Unosquare.RaspberryIO;
+using Unosquare.RaspberryIO.Abstractions;
+
+namespace Node.Tests
+{
+    public class TestTransmitter    
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Assert.That(Unosquare.PiGpio.NativeMethods.Setup.GpioInitialise(), Is.EqualTo(ResultCode.Ok));
+        }
+
+        [Test]
+        public async Task TransmitBitPatterns()
+        {
+            var pin = Board.Pins[UserGpio.Bcm17];
+            var transmitter = new Transmitter433(pin);
+
+            var bits = "10101010";
+            var symbols = bits.SelectMany(bit => 
+                bit == '1' ? 
+                    new [] {new RadioSymbol(650, true), new RadioSymbol(300, false)} : 
+                        new [] {new RadioSymbol(300, true), new RadioSymbol(650, false)}
+                ).ToArray();
+            await transmitter.Transmit(symbols);
+        }
+    }
+}
